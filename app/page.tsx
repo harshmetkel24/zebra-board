f7673619be4f69e561a837d831f53acc5ce82f52
harshmetkel24 <harshmetkel24@gmail.com>
2025-08-25 23:56:18 +0530

    button usage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-  return <h1>Hello Next.js!</h1>;
+  return (
+    <div className="h-screen w-full place-items-center">
+      <h1 className="text-4xl font-bold">Welcome to Zebra Board!</h1>
+      <Button>Click me, Iam shadcn button</Button>
+    </div>
+  );
 }
